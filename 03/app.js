c0c79dcf19e6e435da354e50e5f241ea3dc25a96
loweranterior <jnureport01@gmail.com>
2021-04-08 14:40:42 +0900

    03주차

---
+++ 03/app.js
@@ -0,0 +1,26 @@
+console.log(window.outerWidth);
+
+
+console.log(window.name);
+//window.open('http://www.daum.net');
+
+//var debug = document.getElementById('debug');
+//console.log(debug);
+
+//var box = document.querySelector('#debug');
+//console.log(box);
+
+
+//var box = document.querySelector('.box');
+//var box2 = document.querySelectorAll('.box');
+//console.log(box2);
+//var div = document.createElement('div');
+
+//div.style.border = '1px solid #000';
+//div.innerHTML = 'hello!';
+
+//document.body.appendChild(div);
+
+var debug = document.getElementById('debug');
+debug.remove();
+0
